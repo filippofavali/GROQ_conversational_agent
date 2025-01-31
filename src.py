469fb251c1d051afa81f703064e662531b1ce3d7
filippofavali <favalifilippo@gmail.com>
2025-01-31 17:47:25 +0100

    Updated tagging of emotions to predict emotional state of navel and user from the history of the chat

--- src.py
+++ src.py
@@ -68,7 +68,7 @@ class ConversableAgent:
 
     def define_prompt(self):
         user_input = """
-        Based on the past history of the conversation as below: \n{chat_history}\n
+        Based on the past history of the conversation as below (bypass if empty): \n{chat_history}\n
         Kindly answer to user query, in the language used by the user:\n {input}
         """
 
@@ -110,24 +110,26 @@ class NavelConversableAgent(ConversableAgent):
                          memory_length)
 
         self.facial_expressions = facial_expressions
-        self.facial_tagging_function = self.define_facial_tagging()
-        print("DEBUG")
-        print(self.facial_tagging_function)
-        self.llm = self.llm.bind(functions=[{'facial_tagging': self.facial_tagging_function}])
-        self.chain = self.define_chain()
-        # if self.return_llm:
-        #     self.return_llm = self.chain
+        self.agent_memory = ["none"]
+        self.user_memory = ["none"]
+        self.chat_memories = [self.agent_memory, self.user_memory]
+        self.main_chain = self.define_chain()                               # chain to answer the user
+        self.memory_tagging_chain = self.define_emotional_tagging_chain()   # chain to tag agent and user emotions
+
+    def __call__(self, query):
+
+        response = self.main_chain.invoke({"input": query, "chat_history": self.user_memory})
+        self.update_memory(query=query, response=response)
+
+        return response
+
+    def update_memory(self, query:str, response:str):
+
+        self.agent_memory.append(query)
+        self.user_memory.append(response)
+        [memory.pop(0) for memory in self.chat_memories if len(memory) > self.max_memory]       # FIFO memory handling
 
     def define_facial_tagging(self):
-
-        # fields = {
-        #     expression: (float, Field(
-        #         description=(f"'{expression}' facial tag based on user's query and "
-        #                      "your response, ranging from a minimum of 0.0 "
-        #                      "to a maximum of 0.99 (inclusive).")
-        #     )) for expression in self.facial_expressions
-        # }
-        # tagging_expression = create_model("ExpressionsTagging", **fields)  # creating a base model
 
         class FacialExpressionTag(BaseModel):
             """Tag the piece of text with these facial expressions from 0.0 to 0.99"""
@@ -138,57 +140,35 @@ class NavelConversableAgent(ConversableAgent):
             anger: float = Field(description="Anger facial expression based on user's query and your response.")
             smile: float = Field(description="Smile facial expression based on user's query and your response.")
 
-        return convert_pydantic_to_openai_function(FacialExpressionTag)                     # returning functions to bing
+        # return convert_pydantic_to_openai_function(FacialExpressionTag)               # returning functions to bind
+        return FacialExpressionTag
 
     def define_chain(self):
-        # navel_chain = self.define_prompt() | self.llm | self.define_output_parser()
-        navel_chain = self.define_prompt() | self.llm
+        navel_chain = self.define_prompt() | self.llm | self.define_output_parser()
         return navel_chain
 
-    def __call__(self, query):
+    def memory_prompt(self):
+        user_input = """
+        Based on the history contained into memory buffer: \n{memory_buffer}
+        You are asked to control your own face via emotion tagging facial expression.
+        """
 
-        response = self.chain.invoke({"input": query, "chat_history": self.chat_history})
-        self.chat_history.append({"user": query, "assistant": response})
-        if len(self.chat_history) > self.max_memory:
-            self.chat_history.pop(0)  # FIFO memory buffer strategy
-        return response
+        return ChatPromptTemplate([
+            ("system", self.system_role),
+            ("user", user_input),
+        ])
+
+    def define_emotional_tagging_chain(self):
+        return self.memory_prompt() | self.llm.with_structured_output(self.define_facial_tagging())
+
+    def return_agent_emotion(self):
+        return self.memory_tagging_chain.invoke({"memory_buffer": self.agent_memory})
+
+    def return_user_emotion(self):
+        return self.memory_tagging_chain.invoke({"memory_buffer": self.user_memory})
 
 
 if __name__ == "__main__":
-
-    # """
-    # Usage #1:
-    # Returns a conversational agent in the form of a callable object, to be used in order to have a smooth
-    # multi-language conversation.
-    # """
-    #
-    # print("=== USAGE EXAMPLE #1 ===")
-    # conversational_agent = ConversableAgent(model_name="groq-mixtral")
-    #
-    # print("Starting conversation. Type 'exit' to end.")
-    # while True:
-    #     user_query = input("You: ")
-    #     if user_query.lower() == "exit":
-    #         print("Goodbye!")
-    #         break
-    #     print(conversational_agent(user_query))
-    #
-    #
-    # # Usage 2
-    # """
-    # Usage #2:
-    # Returns an llm model as a langchain runnable via GROQ provider. This can be used as an llm block to be implemented
-    # with other means like function calling, tagging, extraction, and more complex chains.
-    # """
-    # print("=== USAGE EXAMPLE #2 ===")
-    # llm_model = ConversableAgent(model_name="groq-mixtral",
-    #                              return_model=True).return_llm
-    #
-    # # this is going to be a usable LLM returned as a LangChain Runnable, you can use it in more complex and dedicated ways
-    #
-    # response = llm_model.invoke("Whats the capital city of New Mexico?")
-    # print(response.content)
-
 
     """
     Usage with Navel: 
@@ -196,16 +176,22 @@ if __name__ == "__main__":
     """
 
     print("=== NAVEL USAGE EXAMPLE ===")
-    navel_agent = NavelConversableAgent(model_name="deepseek")
+    navel_agent = NavelConversableAgent(model_name="llama")
 
-    # using it as in a chat like - multiple queries in a raw
+    # using it as in a chat like - multiple queries in a raw, every n_step query the Agent returns an emotional status
+    n_step = 2
+    calls_counter = 0
+    print("Simulation starting. Type exit to quit.")
     while True:
         user_query = input("You: ")
         if user_query.lower() == "exit":
             print("Goodbye!")
             break
         print(navel_agent(user_query))
-
+        if calls_counter % n_step == 0:
+            print("Navel emotions: ", navel_agent.return_agent_emotion())
+            print("User emotions: ", navel_agent.return_user_emotion())
+        calls_counter += 1
 
 
 
